7ce1c0234e92acf79c78fc2f8dad08e3357af175
Юрий <polyanskii2118@gmail.com>
2024-06-06 15:13:59 +0600

    test new devcontainer

--- ovhomelab/settings.py
+++ ovhomelab/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-^m^9^usdn-^zrw%2u4utl7vb0mwxohv%k7i2m%v77fb0ec2xdx'
+# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -90,17 +91,27 @@ WSGI_APPLICATION = 'ovhomelab.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB'),
+#         'USER': os.getenv('POSTGRES_USER'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': 'testproject',
+        'USER': 'testproject',
+        'PASSWORD': "hello",
+        'HOST': "postgresdb",
+        'PORT': '5432',
+        'CONN_MAX_AGE': 60,
     }
 }
-
 
 
 # Password validation
